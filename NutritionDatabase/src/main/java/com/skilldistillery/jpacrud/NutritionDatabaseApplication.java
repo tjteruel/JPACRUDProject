308b7ae9232474e7128c212215ecce61bdef2da3
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-03-20 15:19:48 -0600

    stub boot MVC, linked JPA

---
+++ NutritionDatabase/src/main/java/com/skilldistillery/jpacrud/NutritionDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionDatabaseApplication.class, args);
+	}
+
+}
